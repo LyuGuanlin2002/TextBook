e95b29b93b63f072b961c3713eb365ca38deabc2
LyuGuanlin2002 <155526913+LyuGuanlin2002@users.noreply.github.com>
2024-01-03 16:46:42 +0800

    Add files via upload

---
+++ #delete-temp-files.bat
@@ -0,0 +1,5 @@
+@echo off
+del /q *.aux *.bbl *.blg *.log *.out *.toc *.bcf *.xml *.synctex *.glo *.mw *.nlo *.nls *.nlg *.bak *.ind *.idx *.ilg *.lof *.lot *.ent-x *.tmp *.ltx *.los *.lol *.loc *.listing *.gz *.userbak *.nav *.snm *.ptc *.vrb *.synctex(busy) *.records *.brf
+del /q *.ptc
+
+del /q *.nav *.snm *.vrb *.fls *.xdv *.fdb_latexmk
---
+++ #xelatex-minted.bat
@@ -0,0 +1,2 @@
+xelatex -shell-escape test.tex
+makeindex test.nlo -s nomencl.ist -o test.nls -t test.nlg
